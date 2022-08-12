a2fd699af1819350527d55ffccbd8e57270c417d
Dushyant <devops@bydushyant.com>
2022-08-12 23:38:30 +0530

    Get Nevbar and theme switch working

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function enableLightMode() {
 }
 
 function setThemePreference() {
-  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
     enableDarkMode();
     return;
   }
@@ -29,15 +29,18 @@ document.onload = setThemePreference();
 
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
+const mobileNav = (document.getElementById("mobile-nav"));
 
 navToggle.addEventListener("click", () => {
   const visibility = primaryNav.getAttribute("data-visible");
   if (visibility === "false") {
     primaryNav.setAttribute("data-visible", "true");
     navToggle.setAttribute("aria-expanded", "true");
+    mobileNav.setAttribute("d", "m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z");
   }
   if (visibility === "true") {
     primaryNav.setAttribute("data-visible", "false");
     navToggle.setAttribute("aria-expanded", "false");
+    mobileNav.setAttribute("d", "M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z");
   }
 });
