136d997c14d5411905707481fc957a242a73e79f
Dushyant <devops@bydushyant.com>
2022-08-14 00:03:11 +0530

    Make nav sticky on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Theme switch logic
 const themeToggle = document.querySelector("#theme-toggle");
 
 themeToggle.addEventListener("click", () => {
@@ -16,6 +17,8 @@ function enableLightMode() {
   document.body.classList.add("light-theme");
 }
 
+
+// Set initial theme
 function setThemePreference() {
   if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
     enableDarkMode();
@@ -23,24 +26,39 @@ function setThemePreference() {
   }
   enableLightMode();
 }
-
 document.onload = setThemePreference();
 
 
+// Mobile navigation toggle switch logic
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
-const mobileNav = (document.getElementById("mobile-nav"));
+const mobileNavIcon = document.querySelector("#mobile-nav-icon");
 
 navToggle.addEventListener("click", () => {
   const visibility = primaryNav.getAttribute("data-visible");
   if (visibility === "false") {
     primaryNav.setAttribute("data-visible", "true");
     navToggle.setAttribute("aria-expanded", "true");
-    mobileNav.setAttribute("d", "m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z");
+    mobileNavIcon.setAttribute("d", "m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z");
   }
   if (visibility === "true") {
     primaryNav.setAttribute("data-visible", "false");
     navToggle.setAttribute("aria-expanded", "false");
-    mobileNav.setAttribute("d", "M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z");
+    mobileNavIcon.setAttribute("d", "M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z");
   }
 });
+
+// Navbar style on scroll 
+document.onreadystatechange = () => {
+  let lastScrollPosition = 0;
+  const navbar =  document.querySelector(".primary-header");
+
+  window.addEventListener('scroll', function(e) {
+    lastScrollPosition = window.scrollY;
+    
+    if (lastScrollPosition > 50)
+      navbar.classList.add('primary-header-scrolled');
+    else
+      navbar.classList.remove('primary-header-scrolled');
+  });
+}
